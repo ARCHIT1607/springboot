87d083227a0d38875650cfcb8e54488d660c474e
Archit S Maniyath <archit778@gmail.com>
2020-08-02 02:53:25 +0530

    Rest API Created

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -3,26 +3,29 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Person;
 import com.example.repository.UserRepository;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class UserController {
 	
 	@Autowired
 	UserRepository userdao;
 	
-	@RequestMapping("springboot")
+	@RequestMapping("/springboot")
 	public String home() {
 		System.out.println("Hi");
 		return "home.jsp";
 	}
 	
-	@RequestMapping("save")
-	public String addUser(Person person) {
-		userdao.save(person);
-		return "home.jsp";
-	}
+	@PostMapping("/employees")
+    public Person addEmployee(@RequestBody Person employee) {
+        return userdao.save(employee);
+    }
 }
